1167e93baa0cdcd1cebe505a6689552caf0786c7
MuraliPalla-Git <muralidharpalla01@gmail.com>
2026-07-01 17:09:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-0402e980e69d5978b"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "march-kp"
+    key_name = "newacc-SG"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-0a077d772a3f9df8d", "subnet-09815416df6f5ca1e"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -29,16 +29,14 @@ resource "aws_launch_template" "web_server_as" {
 resource "aws_autoscaling_group" "web_server_asg" {
     name                 = "web-server-asg"
     min_size             = 1
-    max_size             = 3
+    max_size             = 4
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    availability_zones    = ["eu-north-1a", "eu-north-1b"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
-      }
-    
-    
+      }    
   }
 
